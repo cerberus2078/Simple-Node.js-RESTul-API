975b0c93ac4a2b12cae859141b101858755c2637
Sara-Sofia Paananen <111874242+cerberus2078@users.noreply.github.com>
2024-04-07 21:36:59 +0300

    API postman stuff

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 
-const products =
+let products =
 [
     {
         id:1,
@@ -48,7 +50,7 @@ const products =
     }
 ]
 
-
+//Handlebar
 app.engine('handlebars',exphbs.engine({
     defaultLayout: 'main'
 }));
@@ -65,8 +67,115 @@ app.get('/', (req,res) => {
     );
 });
 
+
+
+//POSTMAN PART
+// Get all products
+app.get('/products', (req,res) => {
+    res.json(products);
+});
+
+//Get a product based on its ID
+app.get('/products/:id', (req,res) => {
+    const productId = Number(req.params.id);
+    console.log(productId);
+
+    //const car = cars.find(car => car.id === id)
+    const product = products.find(product => product.id === productId);
+
+    if (product){
+    res.json(product);
+    }
+
+    else{
+        res.status(404).json({
+            msg: "Not found"
+        });
+    }
+});
+
+// DELETE
+app.delete('/api/products/:id', (req,res) => {
+    const productId = Number(req.params.id);
+    
+    // check if we have a product with the id
+    const product = products.find(product => product.id === productId);
+
+    if(product){
+        // cars = cars.filter(car => car.id !== idToRemove);
+        products = products.filter(product => product.id !== productId);
+        res.status(200).json({
+            id: productId
+        });  
+    }
+    else{
+        res.status(404).json({
+            msg: 'Could not find the product'
+        });
+    }
+});
+
+// Create 
+app.post('/api/products', (req,res) => {
+    //console.log(req.body);
+    //res.send('ok');
+    const lastId = products[products.length-1].id;
+    const newId = lastId + 1;
+    //console.log(newId);
+
+    //res.send('ok');
+    
+    newProduct = {
+        id: newId,
+        name: req.body.name,
+        genre: req.body.genre,
+        type: req.body.type,
+        reviews: req.body.reviews,
+        price: req.body.price
+    }
+
+    products.push(newProduct);
+
+    res.location('https://localhost:4000/products/' + newId);
+    res.status(201).json(newProduct);
+    
+});
+
+
+// Update
+app.patch('/products/:id', (req,res) => {
+    const idToUpdate = Number(req.params.id);
+    const newName = req.body.name;
+    const newGenre = req.body.genre;
+    const newType = req.body.type;
+    const newReviews = req.body.reviews;
+    const newPrice = req.body.price;
+
+    products.forEach(product => {
+        if(product.id === idToUpdate){
+            product.name = newName;
+            product.genre = newGenre;
+            product.type = newType;
+            product.reviews = newReviews;
+            product.price = newPrice;
+        }
+    });
+
+
+    const product = products.find(product => product.id === idToUpdate);
+    if(product){
+        res.status(200).json(product);
+    }
+    else{
+        res.status(404).json({msg: 'Could not find the product'});
+    }
+});
+
+
+
 app.use(express.static('public'));
 
 
+// To show that the port is running
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
